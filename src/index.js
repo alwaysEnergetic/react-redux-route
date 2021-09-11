a85b3d5549c7bca6fbfcead80db7d8cd85db1b4a
heavenstar627 <kotenkoofficial@gmail.com>
2021-09-11 06:13:06 +0300

    well designed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 const store = createStore (reducers);
+
+
 ReactDOM.render(
   <Provider store = {store}>
     <App />
